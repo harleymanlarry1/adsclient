b79d5a950bbeda0096a86d197ac3bd770a9b5069
roelandmoors <roeland@webding.org>
2012-04-28 13:05:19 +0200

    latest source commit

---
+++ src/AdsClient/Common/IAdsConnectionSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ads.Client.Common
+{
+    public interface IAdsConnectionSettings
+    {
+        string Name { get; set; }
+        string AmsNetIdSource { get; set; }
+        string IpTarget { get; set; }
+        string AmsNetIdTarget { get; set; }
+        ushort AmsPortTarget { get; set; }
+    }
+}
